cdffa986b0cb86d946a31ef6ba0adea1c016d053
Alex Valder <angelaarhangelska@gmail.com>
2024-03-02 19:40:11 +0100

    Label => ProgressBar

--- resources/renewable_stat.gd
+++ resources/renewable_stat.gd
@@ -9,9 +9,10 @@ class_name RenewableStat
         value = new_value
 @export var delay: float = 1.0
 @export var duration: float = 5.0
-@export_node_path("Label") var output: NodePath
+@export_node_path("ProgressBar") var output: NodePath
+@export_color_no_alpha var bar_color: Color = Color.CORAL
 
-var _label: Label = null
+var _bar: ProgressBar = null
 var _tween: Tween = null
 
 
@@ -42,13 +43,27 @@ func stop_recover() -> void:
 
 
 func _calc_dur() -> float:
-    print((max_value - value) * duration / max_value)
     return (max_value - value) * duration / max_value
 
 
 func _set_text(val: float) -> void:
-    if _label:
-        _label.text = "%3.3f" % val
+    if _bar:
+        _bar.value = val
+
+
+func _configure() -> void:
+    _bar.max_value = max_value
+    _bar.min_value = min_value
+    _bar.rounded = false
+
+    var stylebox1 := StyleBoxFlat.new()
+    stylebox1.anti_aliasing = false
+    stylebox1.corner_detail = 1
+    stylebox1.bg_color = bar_color
+    _bar.add_theme_stylebox_override("fill", stylebox1)
+    var stylebox2 := stylebox1.duplicate() as StyleBoxFlat
+    stylebox2.bg_color = bar_color.darkened(0.7)
+    _bar.add_theme_stylebox_override("background", stylebox2)
 
 
 func _ready() -> void:
@@ -58,6 +73,7 @@ func _ready() -> void:
     assert(duration > 0)
 
     if output && !output.is_empty():
-        _label = get_node(output)
-        if is_instance_valid(_label):
+        _bar = get_node(output)
+        if is_instance_valid(_bar):
+            _configure()
             _set_text(value)
